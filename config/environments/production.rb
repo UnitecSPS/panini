c20fa56827c814db9d868b8daa9fe8cd32476c6e
Carlos Gochez <cgotcha@gmail.com>
2014-06-17 11:39:18 -0600

    agregar .erb para recomponer los path de las imagenes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,7 @@ Panini::Application.configure do
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-  config.assets.precompile += %w( external.css *.gif *.png *.jpg )
+  config.assets.precompile += %w( external.css external.js *.gif *.png *.jpg )
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
